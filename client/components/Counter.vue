2fe141be089ef67fe6b8bb6ab4604dd092616236
Jeremy Castagno <JeremyBYU@gmail.com>
2017-07-24 19:11:49 -0400

    Simple Vue VSCode App

---
+++ client/components/Counter.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="counter-wrapper">
+    <div>
+      <img src="https://vuejs.org/images/logo.png" width="200" height="200" />
+    </div>
+  </div>
+</template>
+
+<script>
+// @ts-check
+
+export default {
+  computed: {
+  },
+  methods: {
+    hello () {
+      console.log('hello')
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.counter {
+  margin: 100px auto;
+  border-radius: 3px;
+  width: 200px;
+  height: 200px;
+  text-align: center;
+  line-height: 200px;
+  font-size: 5rem;
+  background-color: #f0f0f0;
+  user-select: none;
+}
+</style>
